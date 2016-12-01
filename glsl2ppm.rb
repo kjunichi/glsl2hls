f3f43e870d0fa3bd4d06f8ec63193f1398de3dc3
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-12-01 11:28:00 +0900

    Update glsl2ppm.rb

--- glsl2ppm.rb
+++ glsl2ppm.rb
@@ -1,3 +1,4 @@
+# Redisの設定
 host     = "127.0.0.1"
 port = 6379
 database = 0
@@ -7,15 +8,19 @@ log = Fluent::Logger.new(nil, :protocol=> 'tcp', :host=> '127.0.0.1', :port=> '2
 r = Redis.new host, port
 r.select database
 
+# ジョブIDを引数でもらう。
 jobId = ARGV[0]
 
+# RedisからジョブIDを指定して該当のGLSLコードを取得する。
 shader = r.get jobId
 h = JSON.parse(shader)
 
+# GLSLを生成、Vertex,Fragmentそれぞれのシェーダープログラムを登録する。
 glsl = Glsl.new
 glsl.attachVertexShader h["v"]
 glsl.attachFragmentShader h["f"]
 
+# ffmpegコマンドのむずかしい設定
 ffmpegCmd = <<"EOT"
 ffmpeg -y -f ppm_pipe \
     -f ppm_pipe -r 24 -i - \
@@ -29,16 +34,15 @@ ffmpeg -y -f ppm_pipe \
 EOT
 
 IO.pipe do |r2, w|
-  #IO.popen("#{ffmpegCmd} 2>&1","r", err:w) do |pipe|
-    IO.popen("#{ffmpegCmd} 2>&1","w", err:w) do |pipe|
+  IO.popen("#{ffmpegCmd} 2>&1","w", err:w) do |pipe|
+    # 秒間24フレームで15秒作成する。
     (24*15).times {|i|
+      # GLSLをレンダリングする
       pipe.puts glsl.render
     }
+    # ffmpegへの書き込みパイプを閉じる。
     w.close
-    #pipe.close_write
-    end
-  #puts pipe.gets
-  
+  end　
+  # ffmpegの標準出力、エラー出力をfluentdへ投げる
   log.post('glsl2ppm', {"log" => r2.read})
 end
-
