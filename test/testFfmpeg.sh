55fed9347ed05010fcd3b835e5158533be0ff4ed
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-12-09 18:40:33 +0900

    Update testFfmpeg.sh

--- test/testFfmpeg.sh
+++ test/testFfmpeg.sh
@@ -1,5 +1,5 @@
 cat static/test*.ppm|ffmpeg -y \
-    -f image2 -r 24 -i - \
+    -f image2pipe -r 24 -i - \
     -f ssegment -segment_format mpegts -s 320x240 -r 24 -c:v libx264 \
     -pix_fmt yuv420p \
     -flags +loop-global_header -bsf h264_mp4toannexb \
