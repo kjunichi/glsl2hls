55a0a29b11dfb357140ae6a21b8f56e19870bbee
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-12-09 19:17:35 +0900

    Update testFfmpeg.sh

--- test/testFfmpeg.sh
+++ test/testFfmpeg.sh
@@ -4,5 +4,5 @@ cat static/test*.ppm|avconv -y -f image2pipe -r 24 -i - \
     -flags +loop-global_header -bsf h264_mp4toannexb \
     -strict experimental \
     -segment_list static/test.m3u8 \
-    -break_non_keyframes 1 -segment_list_type hls \
+    -segment_list_type hls \
     -segment_time 8 -segment_list_size 3 -segment_list_flags +live -threads 4 static/test%03d.ts
