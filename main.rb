a51363eb163288f38148f7f8cb77ae6e387c8a9b
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-12-09 13:35:00 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -15,18 +15,7 @@ def doConvert(r,j)
   h = JSON.parse(j)
   puts h["Id"]
   r.set h["Id"], j
-  ffmpegCmd = <<"EOT"
-ffmpeg -y -f ppm_pipe \
--f ppm_pipe -r 24 -i - \
--f ssegment -segment_format mpegts -s 320x240 -r 24 -c:v libx264 \
--pix_fmt yuv420p \
--flags +loop-global_header -bsf h264_mp4toannexb \
--strict experimental \
--segment_list static/#{h["Id"]}.m3u8 \
--break_non_keyframes 1 -segment_list_type hls \
--segment_time 8 -segment_list_size 3 -segment_list_flags +live -threads 4 static/#{h["Id"]}%03d.ts
-EOT
-
+  
   `mruby/bin/mruby glsl2ppm.rb #{h["Id"]}`
   puts "doConvert end"
 end
