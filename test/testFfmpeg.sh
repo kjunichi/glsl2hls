4a25e94d1f4c455709babfdca5c51bcedae1cd5f
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-12-09 17:36:43 +0900

    Create testFfmpeg.sh

---
+++ test/testFfmpeg.sh
@@ -0,0 +1,9 @@
+cat static/test*.ppm|ffmpeg -y -f ppm_pipe \
+    -f ppm_pipe -r 24 -i - \
+    -f ssegment -segment_format mpegts -s 320x240 -r 24 -c:v libx264 \
+    -pix_fmt yuv420p \
+    -flags +loop-global_header -bsf h264_mp4toannexb \
+    -strict experimental \
+    -segment_list static/test.m3u8 \
+    -break_non_keyframes 1 -segment_list_type hls \
+    -segment_time 8 -segment_list_size 3 -segment_list_flags +live -threads 4 static/test%03d.ts
