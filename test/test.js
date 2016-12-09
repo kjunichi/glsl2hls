7fe5890989a66c8cb6752c382d20fa706948dd70
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-12-09 11:48:26 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,30 +3,31 @@ const Nightmare = require('nightmare')
 
 describe('GLSL start page.', () => {
   describe('index.html', () => {
-    it('should index.html', () => {
+    it('should index.html', (done) => {
       const nightmare = Nightmare({
           show: false,
           switches: {'ignore-certificate-errors': 'true' }
       })
-    nightmare
-      .viewport(1024, 768)
-      .on('console',(type,args) =>{
-        console.log(args)
-      })
-      .goto('http://localhost:8000/static/index.html')
-      .evaluate(()=>{
-        return document.body.innerHTML
-      },(result) => {
-        console.log(`result = ${result}`)
-        assert.equal('html',result)
-      })
-      .end()
-      .then(function (result) {
-        console.log(result)
-      })
-      .catch(function (error) {
-        console.error('index.htlm failed:', error);
-      })
+      nightmare
+        .viewport(1024, 768)
+        .on('console',(type,args) =>{
+          console.log(args)
+        })
+        .goto('http://localhost:8000/static/index.html')
+        .evaluate(()=>{
+          return document.body.innerHTML
+        },(result) => {
+          console.log(`result = ${result}`)
+          assert.equal('html',result)
+          done()
+        })
+        .end()
+        .then(function (result) {
+          console.log(result)
+        })
+        .catch(function (error) {
+          console.error('index.htlm failed:', error);
+        })
   
     })
   })
