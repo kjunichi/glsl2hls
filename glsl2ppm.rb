65a9c349282d7cbe57bde05f9196851605ecdaf8
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2016-12-09 19:02:27 +0900

    Update glsl2ppm.rb

--- glsl2ppm.rb
+++ glsl2ppm.rb
@@ -26,8 +26,7 @@ glsl.attachFragmentShader h['f']
 
 # ffmpegコマンドのむずかしい設定
 ffmpegCmd = <<"EOT"
-ffmpeg -y -f ppm_pipe \
-    -f ppm_pipe -r 24 -i - \
+ffmpeg -y -f ppm_pipe -r 24 -i - \
     -f ssegment -segment_format mpegts -s 320x240 -r 24 -c:v libx264 \
     -pix_fmt yuv420p \
     -flags +loop-global_header -bsf h264_mp4toannexb \
